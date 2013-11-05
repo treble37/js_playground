af0bdea56ccec0e09f62455d3738fff3c5585ed4
BruceP <brucepark0@gmail.com>
2013-11-05 08:43:14 -0800

    added js_experiments

---
+++ js_experiments/js_experiments.js
@@ -0,0 +1,17 @@
+//setup prototype
+function FunctionObjectProps() {
+  this.function_call  = null;
+}
+
+function msg_alert(msg) {
+  alert(msg+" love");
+}
+
+function init_msg_alert(fobj,func_name) {
+  fobj.function_call = func_name;
+}
+$( document ).ready(function() {
+  var fop = new FunctionObjectProps();
+  init_msg_alert(fop,msg_alert("someday"));
+  fop.function_call("hello");
+});
